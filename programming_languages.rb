e6e59597d04fdf0de31b733dc6023fcc7e6e1213
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-11 17:28:24 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,10 +4,13 @@ def reformat_languages(languages)
   
   languages.each {|category,list|
   list.each{|language,kind|
-      new_hash[language]={type: kind[:type]}
+   if new_hash[language].nil?
+     new_hash[language]={type: kind[:type]}
+   end
       if new_hash[language][:style].nil?
           new_hash[language][:style] =[]
       end
+      
         new_hash[language][:style] << category
       puts new_hash[language]
     }
